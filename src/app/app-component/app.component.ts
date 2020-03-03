711237aec2b174d27c8e047be91e39620d1ddd2c
Alverson Layne <alverson.layne2@gmail.com>
2020-03-03 12:41:32 -0500

    Created sortItems method in app component ts to sort listItems alphabetically.

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -11,22 +11,60 @@ library.add(faTimes, faPlus);
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  title = 'Wisdom Pet Medicine';
   theList: object[];
+  modifiedList: object[];
+  orderBy: string;
+  orderType: string;
 
   addApt(theApt: any) {
     this.theList.unshift(theApt);
+    this.modifiedList.unshift(theApt);
   }
 
   deleteApt(theApt: object) {
     this.theList = without(this.theList, theApt);
+    this.modifiedList = without(this.theList, theApt);
   }
 
-  constructor(private http: HttpClient) {}
+  searchApt(theQuery: string) {
+    this.modifiedList = this.theList.filter(eachItem => {
+    return (
+      eachItem['petName'].toLowerCase().includes(theQuery.toLowerCase()) ||
+      eachItem['ownerName'].toLowerCase().includes(theQuery.toLowerCase()) ||
+      eachItem['aptNotes'].toLowerCase().includes(theQuery.toLowerCase())
+      );
+    });
+    this.sortItems();
+  }
+
+  sortItems() {
+    let order: number;
+    if (this.orderType === 'asc') {
+        order = 1;
+      } else {
+        order = -1;
+      }
+
+    this.modifiedList.sort((a, b) => {
+      if (a[this.orderBy].toLowerCase() < b[this.orderBy].toLowerCase()) {
+        return -1 * order;
+      }
+      if (a[this.orderBy].toLowerCase() > b[this.orderBy].toLowerCase()) {
+        return 1 * order;
+      }
+    });
+  }
+
+  constructor(private http: HttpClient) {
+    this.orderBy = 'petName';
+    this.orderType = 'asc';
+  }
 
   ngOnInit(): void {
     this.http.get<any>('../assets/data.json').subscribe(data =>{
       this.theList = data;
+      this.modifiedList = data;
+      this.sortItems();
       console.log(data);
     });
   }
